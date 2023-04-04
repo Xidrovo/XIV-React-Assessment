ede91f75856969ffa6049b5b69d3abbdfb538e15
Xavier Idrovo <xidrovo@hotmail.com>
2023-04-03 21:59:31 -0500

    Creating a test suite

---
+++ ninjaone-client/jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testMatch: [
+    '**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '**/*.{spec,test}.{js,jsx,ts,tsx}',
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['./jest.setup.cjs'],
+  testEnvironment: 'jest-environment-jsdom',
+};
